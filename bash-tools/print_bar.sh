c25f70fcd3289480caf516d9589aa6160cd403be
Andres Gongora <mail@andresgongora.com>
2020-04-13 18:26:58 +0200

    Remove color dependency
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- bash-tools/print_bar.sh
+++ bash-tools/print_bar.sh
@@ -26,21 +26,21 @@
 ##	printBar(CURRENT, MAX, SIZE, COLOR, COLOR)
 ##
 ##	Prints a bar that is filled depending on the relation between
-##	CURRENT and MAX.
+##	CURRENT and MAX. Example: [|||||    ]
+##
 ##
 ##	Arguments:
 ##	1. CURRENT:       amount to display on the bar.
 ##	2. MAX:           amount that means that the bar should be printed
 ##	                  completely full.
 ##	3. SIZE:          length of the bar as number of characters.
-##	4. BRACKET_COLOR: Color for the brackets. May be empty for not colored.
-##	5. BAR_COLOR:	  Color for the bars. May be empty for not colored.
 ##
-##	Optional:
-##	6. BRACKET_CHAR_L: left bracket character. Defaults to '['
+##
+##	Optional arguments:
+##	4. BRACKET_CHAR_L: left bracket character. Defaults to '['
+##	5. BAR_FILL_CHAR:  bar character. Defaults to '|'
+##	6. BAR_EMPTY_CHAR: bar background. Defaults to ' '
 ##	7. BRACKET_CHAR_R: left bracket character. Defaults to ']'
-##	8. BAR_FILL_CHAR:  bar character. Defaults to '|'
-##	8. BAR_EMPTY_CHAR: bar background. Defaults to ' '
 ##
 printBar()
 {
@@ -48,12 +48,10 @@ printBar()
 	local current=$1
 	local max=$2
 	local size=$3
-	local bracket_color=$4
-	local bar_color=$5
-	local bracket_char_l=${6:-'['}
+	local bracket_char_l=${4:-'['}
+	local bar_fill_char=${5:-'|'}
+	local bar_empty_char=${6:-' '}
 	local bracket_char_r=${7:-']'}
-	local bar_fill_char=${8:-'|'}
-	local bar_empty_char=${9:-' '}
 
 
 	## COMPUTE VARIABLES
@@ -69,7 +67,7 @@ printBar()
 	## - Fill bars
 	## - Remaining empty background
 	## - Closing bracket
-	printf "$bracket_color$bracket_char_l$bar_color"
+	printf "$bracket_char_l"
 	i=0
 	while [ $i -lt $num_bars ]; do
 		printf "$bar_fill_char"
@@ -79,5 +77,5 @@ printBar()
 		printf "$bar_empty_char"
 		i=$[$i+1]
 	done
-	printf "$bracket_color$bracket_char_r$fc_none"
+	printf "$bracket_char_r"
 }
