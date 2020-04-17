32fde20cdca37d54baeb1ab441f20e830d0147d1
Andres Gongora <mail@andresgongora.com>
2020-04-17 10:47:33 +0200

    Fix issue with recursive include, baby one more time
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- bash-tools/hook_script.sh
+++ bash-tools/hook_script.sh
@@ -24,7 +24,8 @@
 ##
 ##
 ##
-include(){ [ -z "$_IR" ]&&_IR="$PWD"&&cd $( dirname "$PWD/$0" )&&. "$1"&&cd "$_IR"&&unset _IR||. $1;}
+include(){ { [ -z "$_IR" ]&&_IR="$PWD"&&cd "$(dirname "$PWD/$0")"&&include "$1"&&cd "$_IR"&&unset _IR;}||{ local d=$PWD&&cd "$(dirname "$PWD/$1")"&&. "$(basename "$1")"&&cd "$d";}||{ echo "Include failed $PWD->$1"&&exit 1;};}
+
 include 'edit_text_file.sh'
 include 'shell.sh'
 
