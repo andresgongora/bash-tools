6e65fa2523eaa21f1199c0e9ce3f6b1b218330e1
Andres Gongora <mail@andresgongora.com>
2020-06-18 11:20:05 +0200

    Only hook script for interactive sessions
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- bash-tools/check_interactive_session.sh
+++ bash-tools/check_interactive_session.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 ##  +-----------------------------------+-----------------------------------+
 ##  |                                                                       |
-##  | Copyright (c) 2019-2020, Andres Gongora <mail@andresgongora.com>.     |
+##  | Copyright (c) 2020, Andres Gongora <mail@andresgongora.com>.          |
 ##  |                                                                       |
 ##  | This program is free software: you can redistribute it and/or modify  |
 ##  | it under the terms of the GNU General Public License as published by  |
@@ -19,38 +19,35 @@
 ##  |                                                                       |
 ##  +-----------------------------------------------------------------------+
 
-##
-##	DESCRIPTION
-##
-##
-##
-[ "$(type -t include)" != 'function' ]&&{ include(){ { [ -z "$_IR" ]&&_IR="$PWD"&&cd $(dirname "${BASH_SOURCE[0]}")&&include "$1"&&cd "$_IR"&&unset _IR;}||{ local d=$PWD&&cd "$(dirname "$PWD/$1")"&&. "$(basename "$1")"&&cd "$d";}||{ echo "Include failed $PWD->$1"&&exit 1;};};}
 
-include 'edit_text_file.sh'
-include 'shell.sh'
+##
+##
+##
+##
+##
+##
+##
+
+
 
 ##==============================================================================
 ##
-##	
-hookScript()
-{
-	local script="$1"
-	local script_name=$(basename "$script")
-		
+##==============================================================================
 
-	local hook=$(printf '%s'\
-	"\n"\
-	"##-----------------------------------------------------\n"\
-	"## ${script_name}\n"\
-	"if [ -f ${script} ] && [ -n \"\$( echo \$- | grep i )\" ]; then\n"\
-	"\tsource ${script}\n"\
-	"fi")
+## If not running interactively, don't do anything
+[[ $- != *i* ]] && return
 
 
-	## ADD TO RC FILE
-	local user_shell=$(getShellName)
-	editTextFile $(getUserRCFile) append "$hook"
-}
+
+
+
+
+##==============================================================================
+##	TEST
+##==============================================================================
+
+#getShellName
+
 
 
 
