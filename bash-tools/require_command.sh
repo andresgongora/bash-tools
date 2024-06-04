c8f79b0241310cc8a442ebfb063f0eb3d13abcc2
Andres Gongora <mail@andresgongora.com>
2024-06-04 11:36:31 +0200

    Add require_command
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- bash-tools/require_command.sh
+++ bash-tools/require_command.sh
@@ -23,46 +23,7 @@
 ##==================================================================================================
 ## @brief Check if command is not available and exit if so.
 ##
+## @param $1: command to check for
+## @exit 1 if command is not available, continues otherwsie.
+## @example exitIfCommandsNotAvailable nano
 requireCommand() { command -v $1 >/dev/null || { echo "Aborting: '$1' not found"; exit 1; }; }
-## @todo: remove, there is already a dedicated "require_command" script in bash-tools!
-
-
-##==================================================================================================
-## @brief Check if any of the commands passed as arguments are not available.
-##
-## Check if any of the commands passed as arguments are not available. If any
-## command is not available, print a warning and call 'exit 1'.
-##
-## @param $@: list of commands to check.
-## @exit 1 if any command is not available, continues otherwsie.
-## @example exitIfCommandsNotAvailable nano ssh
-##
-exitIfCommandsNotAvailable() {
-	local num_missing=0
-
-	## Iterate over all commands to check
-	for maybe_command in "$@"; do
-		if [ -z $(command -v ${maybe_command}) ]; then
-			echo "'${maybe_command}' not found."
-			num_missing=$((num_missing+1))
-		fi
-	done
-
-	## Abort if any package was missing
-	if [ $num_missing -gt 0 ]; then
-		echo "Aborting: $num_missing commands were missing. Please install needed packages."
-		exit 1
-	fi
-}
-
-
-##==================================================================================================
-##	DEBUG
-##==================================================================================================
-
-## This should work
-#exitIfCommandsNotAvailable nano # 1 command
-#exitIfCommandsNotAvailable nano ssh # 2 commands
-
-## This should fail
-#exitIfCommandsNotAvailable random-command-name another-madeup-command
